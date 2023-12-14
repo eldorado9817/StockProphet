40e008ff741f83c3159b66a4703819ed79e331c2
James Johnson <eldorado9817@gmail.com>
2023-12-14 20:58:57 +0000

    complete

---
+++ LSTM.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import yfinance as yf
+import pandas_ta as ta
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+if torch.backends.mps.is_available():
+    device = torch.device('mps')
+    
+data = yf.download(tickers = '^RUI', start = '2012-03-11',end = '2022-07-10')
+data.head(10)
+
+# Adding indicators
+data['RSI'] = ta.rsi(data.Close, length=15)
+data['EMAF'] = ta.ema(data.Close, length=20)
+data['EMAM'] = ta.ema(data.Close, length=100)
+data['EMAS'] = ta.ema(data.Close, length=150)
+
+data['Target'] = data['Adj Close'] - data.Open
+data['Target'] = data['Target'].shift(-1)
+
+data['TargetClass'] = [1 if data.Target[i] > 0 else 0 for i in range(len(data))]
+
+data['TargetNextClose'] = data['Adj Close'].shift(-1)
+
+data.dropna(inplace=True)
+data.reset_index(inplace=True)
+data.drop(['Volume', 'Close', 'Date'], axis=1, inplace=True)
+
+data_set = data.iloc[:, 0:11]
+
+pd.set_option('display.max_columns', None)
+
+data_set.head(20)
+
+from sklearn.preprocessing import MinMaxScaler
+
+sc = MinMaxScaler(feature_range=(0, 1))
+data_set_scaled = sc.fit_transform(data_set)
+print(data_set_scaled)
+
+# multiple feature from data provided to the model
+X = []
+backcandles = 30
+print(data_set_scaled.shape[0])
+
+for j in range(8):
+    X.append([])
+    for i in range(backcandles, data_set_scaled.shape[0]):
+        X[j].append(data_set_scaled[i - backcandles:i, j])
+
+X = np.moveaxis(X, [0], [2])
+X, yi = np.array(X), np.array(data_set_scaled[backcandles:, -1])
+y = np.reshape(yi, (len(yi), 1))
+
+splitlimit = int(len(X) * 0.8)
+X_train, X_test = X[:splitlimit], X[splitlimit:]
+y_train, y_test = y[:splitlimit], y[splitlimit:]
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc1(out[:, -1, :])
+        return out
+
+
+
+input_dim = 8
+hidden_dim = 150
+num_layers = 1
+output_dim = 1
+
+model = LSTM(input_dim, hidden_dim, num_layers, output_dim)
+
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+epochs = 30
+losses = []
+
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    outputs = model(torch.Tensor(X_train))
+    loss = criterion(outputs, torch.Tensor(y_train))
+    loss.backward()
+    optimizer.step()
+    if epoch % 5 == 0:
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, epochs, loss.item()))
+
+# Convert numpy arrays to PyTorch tensors
+X_test_tensor = torch.Tensor(X_test)
+y_test_tensor = torch.Tensor(y_test)
+
+# Make predictions on the test set
+with torch.no_grad():
+    y_pred_tensor = model(X_test_tensor)
+    y_pred = y_pred_tensor.cpu().numpy()
+
+# Plot the results
+plt.figure(figsize=(16,8))
+plt.plot(y_test, color='black', label='Test')
+plt.plot(y_pred, color='green', label='Pred')
+plt.legend()
+plt.show()
+
